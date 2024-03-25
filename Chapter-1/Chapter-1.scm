ae2d013ce509089e0e86159b7ace8d4ad2453eb6
ErkamUzuncayir <erkamuzuncayir@gmail.com>
2024-03-26 01:59:00 +0300

    solve exercise 1.1 and 1.2

---
+++ Chapter-1/Chapter-1.scm
@@ -0,0 +1,61 @@
+// Exercise 1.1. Below is a sequence of expressions. What is the result
+printed by the interpreter in response to each expression? Assume
+that the sequence is to be evaluated in the order in which it is
+presented.
+
+10
+> 10
+
+(+ 5 3 4)
+> 12
+
+(- 9 1)
+> 8
+
+(/ 6 2)
+> 3
+
+(+ (* 2 4) (- 4 6))
+> 6
+
+(define a 3)
+>
+
+(define b (+ a 1))
+>
+
+(+ a b (* a b))
+> 19
+
+(= a b)
+>
+
+(if (and (> b a) (< b (* a b)))
+    b 
+    a
+    )
+> 4
+
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+        (else 25))
+> 16
+
+(+ 2 (if (> b a) b a))
+> 6
+
+(* (cond ((> a b) a)
+        ((< a b) b)
+        (else -1))
+    (+ a 1))
+> 16
+
+
+// Exercise 1.2
+
+(/ (+ 5 4 (- 2 (-3 (+ 6 + (/ 4 5)))))(* 3 (- 6 2)(- 2 7)))
+
+// Exercise 1.3
+(define x y z)
+    (if (> x y) (> x z) 
+        )
